7f5a17f01ccc780931b21453d7b0c5bb796c3c1a
jehmatthews <jehmatthews30@gmail.com>
2022-08-11 14:23:24 -0700

    template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("😃 Assertion Passed: " + actual + " === " + expected);
+    console.log(`😃 Assertion Passed: ${actual} === ${expected}`);
   } else if (actual !== expected) {
-    console.log("❌ Assertion Failed: " + actual + "!==" + expected);
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 

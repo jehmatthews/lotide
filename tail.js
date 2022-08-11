aad0475c0d7b125d119f80678911c1e6d7a034f5
jehmatthews <jehmatthews30@gmail.com>
2022-08-11 16:45:37 -0700

    slice for tail

---
+++ tail.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😃 Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(array) {
+  return array.slice(1);
+};
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
+
+console.log(words.slice(1));

0fcafe7303e9cf8fc6027b2e30f0879dc42d6428
jehmatthews <jehmatthews30@gmail.com>
2022-08-11 13:53:40 -0700

    assert assignment

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("😃 Assertion Passed: " + actual + " === " + expected);
+  } else if (actual !== expected) {
+    console.log("❌ Assertion Failed: " + actual + "!==" + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);

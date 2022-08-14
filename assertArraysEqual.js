750e5b276b8e59a049c07c25255b9c37c4b1129d
jehmatthews <jehmatthews30@gmail.com>
2022-08-14 14:08:24 -0700

    implementing eq within new function

---
+++ assertArraysEqual.js
@@ -0,0 +1,19 @@
+const eqArrays = function(array1, array2) {
+  let result = true;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] === array2[i]) {
+      return true;
+    } else if (array1 !== array2) {
+      return false
+    }
+  }
+  return result
+};
+
+const assertArraysEqual = function (array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`Assertion Passed`);
+  } else {
+    console.log(`Assertion Failed`);
+  }
+};

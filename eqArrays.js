55542723f2101c6f0876abce870aba7f922e80a0
jehmatthews <jehmatthews30@gmail.com>
2022-08-15 13:33:49 -0700

    updates to function to fix bugs

--- eqArrays.js
+++ eqArrays.js
@@ -8,14 +8,16 @@ const assertEqual = function(actual, expected) {
 
 const eqArrays = function(array1, array2) {
   let result = true;
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] === array2[i]) {
-      return true;
-    } else if (array1 !== array2) {
-      return false
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+      return false;
+      } 
     }
+    return result;
+  } else {
+    return false;
   }
-  return result
 };
 
 eqArrays([1, 2, 3], [1, 2, 3]), true // => true

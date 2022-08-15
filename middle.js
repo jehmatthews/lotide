7287ab53df8147d982e438cf66795aea7a1754c9
jehmatthews <jehmatthews30@gmail.com>
2022-08-15 13:34:12 -0700

    updates to function

--- middle.js
+++ middle.js
@@ -1,13 +1,15 @@
 const eqArrays = function(array1, array2) {
   let result = true;
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] === array2[i]) {
-      return true;
-    } else if (array1 !== array2) {
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
       return false;
+      } 
     }
+    return result;
+  } else {
+    return false;
   }
-  return result;
 };
 
 const assertArraysEqual = function (array1, array2) {
@@ -22,20 +24,19 @@ const middle = function(array) {
   const midItem = array.length / 2;
 
   if (array.length === array[0] || array.length === array[1]) {
-    return ("[]");
+    return [];
   } else if (array.length % 2 === 1) {
-    return array[Math.floor(midItem)];
+    return [array[Math.floor(midItem)]];
   } else {
     return [array[midItem - 1], array[midItem]];
   }
 };
 
+assertArraysEqual(middle([1]), []); // => []
+assertArraysEqual(middle([1, 2]), []); // => []
 
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
+assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
 
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-
-console.log(middle([1, 2, 3, 4]));// => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);// => [2, 3]
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]

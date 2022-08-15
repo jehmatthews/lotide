c381c97fb235e64a0996398d5e134128e61df0c0
jehmatthews <jehmatthews30@gmail.com>
2022-08-14 19:07:51 -0700

    flattening arrays

---
+++ flatten.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😃 Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  let result = true;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] === array2[i]) {
+      return true;
+    } else if (array1 !== array2) {
+      return false
+    }
+  }
+  return result
+};
+
+const flatten = function(array) {
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      result = result.concat(flatten(array[i]));
+    } else {
+      result.push(array[i]);
+    }
+  }
+  return result;
+};
+console.log(flatten([1, 2, [3, 4], 5, [6]]));

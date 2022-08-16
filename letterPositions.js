b17de17861133164575cf5886f6dd037791be027
jehmatthews <jehmatthews30@gmail.com>
2022-08-16 14:24:34 -0700

    updated comments

--- letterPositions.js
+++ letterPositions.js
@@ -22,17 +22,17 @@ const eqArrays = function(array1, array2) {
 
 const letterPositions = function(sentence) {
   const results = {};
-  for (let i = 0; i < sentence.length; i++) {
-    let character = sentence[i];
-    if (character !== ' ') {
-      if (!results[character]) {
-        results[character] = [i];
+  for (let i = 0; i < sentence.length; i++) { //looping through "hello"
+    let character = sentence[i]; // character equals letters found in sentence(hello)
+    if (character !== ' ') { // if not a space, move to next if
+      if (!results[character]) { // if character ex."h", is not found, next line
+        results[character] = [i]; // add the "h" to array in object "results"
       } else {
-        results[character].push(i)
+        results[character].push(i) // if character is found, ex."l", add another index number to "l" array ex [2] -> [2, 3]
       }
     } 
   }
-  return results
+  return results // return the object with arrays
 };
 
 console.log(letterPositions("hello"));

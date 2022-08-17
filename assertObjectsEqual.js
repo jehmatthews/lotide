2f55261ea7ac97f81fd224ebedc78ba9e067f804
jehmatthews <jehmatthews30@gmail.com>
2022-08-17 14:42:00 -0700

    implement this function with use of eqObjects

---
+++ assertObjectsEqual.js
@@ -0,0 +1,29 @@
+const eqObjects = function(object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+  for (let key of keys1) {
+    if (Array.isArray(object1[key]) && (Array.isArray(object2[key]))) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    }
+    else if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+const assertObjectsEqual = function (actual, expected) {
+  if (eqObjects(actual, expected)) {
+    console.log('Assertion Passed', actual, expected);
+  } else {
+    console.log(`Assertion Failed`, actual, expected);
+  }
+};
+
+assertObjectsEqual({a: "1", b: "2"}, {a: "1", b: "2"});

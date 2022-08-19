4c3c960882103967e93d0f31bf03ee02105db306
jehmatthews <jehmatthews30@gmail.com>
2022-08-19 12:05:08 -0700

    updates

--- takeUntil.js
+++ takeUntil.js
@@ -1,3 +1,23 @@
+const eqArrays = function(array1, array2) {
+  let result = true;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] === array2[i]) {
+      return true;
+    } else if (array1 !== array2) {
+      return false
+    }
+  }
+  return result
+};
+
+const assertArraysEqual = function (array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`Assertion Passed`);
+  } else {
+    console.log(`Assertion Failed`);
+  }
+};
+
 const data = ["may", "the", "force", "be", "with", "you"];
 
 const takeUntil = function(array, callback) {
@@ -14,3 +34,5 @@ const takeUntil = function(array, callback) {
 
 const results2 = takeUntil(data, i => i === "be")
 console.log(results2);
+
+assertArraysEqual(results2, ["may", "the", "force"]);

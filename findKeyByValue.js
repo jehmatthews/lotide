174003264fe19f71aa3db0247a7340c3fecd83da
jehmatthews <jehmatthews30@gmail.com>
2022-08-30 14:33:03 -0700

    updates for lotide

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -8,7 +8,7 @@ const assertEqual = function(actual, expected) {
 
 const findKeyByValue = function(object, value) { 
   const keys = Object.keys(object); // variable that takes in all keys of object ex. bestTVShowsByGenre
-  for (i = 0; i < keys.length; i++) { // loops through said object
+  for (let i = 0; i < keys.length; i++) { // loops through said object
     if (value === object[keys[i]]) { // if the value ex. "drama" == the key index..
       return keys[i]; // return that key index
     }
@@ -31,3 +31,5 @@ assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
 assertEqual(findKeyByValue(bestMoviesByGenre, "Interstellar"), "sci_fi");
 assertEqual(findKeyByValue(bestMoviesByGenre, "Star Trek"), undefined);
+
+module.exports = findKeyByValue;

52645559cd4939088ceb8bd7e8f2e8792ba87e65
jehmatthews <jehmatthews30@gmail.com>
2022-08-29 23:04:27 -0700

    writing tests with mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,11 +1,14 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
-
-assertArraysEqual(middle([1]), []); // => []
-assertArraysEqual(middle([1, 2]), []); // => []
-
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);// => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
+// tests that check that the middle element or elements are returned
+describe("#middle", () => {
+  it("returns [] for [1]", () => { // return emptay arr for only 1 or 2 element w/ no middle
+    assert.deepEqual(middle([1]), []);
+  });
+  it("returns [2] for [1, 2, 3]", () => { // returns 1 element when arr.length is odd
+    assert.deepEqual(middle([1, 2 ,3]), [2]);
+  });
+  it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => { // returns 2 elements when arr.length is even
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+});

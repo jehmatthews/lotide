41bfe0ea7105a208acf54265397554d2e4064fa1
jehmatthews <jehmatthews30@gmail.com>
2022-08-30 14:10:46 -0700

    updates for lotide

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😃 Assertion Passed: ${actual} === ${expected}`);
+const assertEqual = function(actual, expected) { // takes in 2 parameters, compares your actual to what you expect
+  if (actual === expected) { // the comparison
+    console.log(`😃 Assertion Passed: ${actual} === ${expected}`); //passes if true
   } else if (actual !== expected) {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);// fails if unequal
   }
 };
 

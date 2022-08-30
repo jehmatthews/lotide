7219b64963614a7ed860c5cfcff66a40ef5f74bb
jehmatthews <jehmatthews30@gmail.com>
2022-08-29 17:53:50 -0700

    adding module exports

--- middle.js
+++ middle.js
@@ -1,25 +1,3 @@
-const eqArrays = function(array1, array2) {
-  let result = true;
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-      return false;
-      } 
-    }
-    return result;
-  } else {
-    return false;
-  }
-};
-
-const assertArraysEqual = function (array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`Assertion Passed`);
-  } else {
-    console.log(`Assertion Failed`);
-  }
-};
-
 const middle = function(array) {
   const midItem = array.length / 2;
 
@@ -32,11 +10,4 @@ const middle = function(array) {
   }
 };
 
-assertArraysEqual(middle([1]), []); // => []
-assertArraysEqual(middle([1, 2]), []); // => []
-
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);// => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
+module.exports = middle;

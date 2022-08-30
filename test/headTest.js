afaec8ba1f19f7fad1da0cd3abe4cae1d1674e04
jehmatthews <jehmatthews30@gmail.com>
2022-08-29 22:18:58 -0700

    updates

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,6 @@
 const assert = require('chai').assert;
 const head   = require('../head');
-
+// tests that ensure first element of array is returned
 describe("#head", () => {
   it("returns 1 for [1, 2, 3]", () => {
     assert.strictEqual(head([1, 2, 3]), 1);

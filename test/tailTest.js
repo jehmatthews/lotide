fd393a04c5763d1453115c5791bbdadbef7c654f
jehmatthews <jehmatthews30@gmail.com>
2022-08-29 22:16:02 -0700

    writing tests with mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,14 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
-
-console.log(tail(words));
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Yo Yo', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
+  it("returns [2, 3, 4, 5] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(tail([1, 2, 3, 4, 5]), [2, 3, 4, 5]);
+  });
+  it("returns ['test', 'test'] for ['test', 'test', 'test']", () => {
+    assert.deepEqual(tail(['test', 'test', 'test']), ['test', 'test']);
+  });
+});

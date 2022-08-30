924f7e6cec0b4377e6028b2214772a758ca53469
jehmatthews <jehmatthews30@gmail.com>
2022-08-30 14:08:31 -0700

    updates for lotide

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,10 +1,10 @@
 const eqArrays = require('./eqArrays');
 
-const assertArraysEqual = function (array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`Assertion Passed`);
+const assertArraysEqual = function (array1, array2) { // takes in 2 arrays as parameters
+  if (eqArrays(array1, array2)) { // eqArrays function comes in and compares our arrays
+    console.log(`Assertion Passed`); // assertion passes if arrays equal
   } else {
-    console.log(`Assertion Failed`);
+    console.log(`Assertion Failed`);// assertion fails if arrays unequal
   }
 };
 

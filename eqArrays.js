9cba784cd4532a153625eba48241d9c967078f28
jehmatthews <jehmatthews30@gmail.com>
2022-08-30 14:28:02 -0700

    updates for lotide

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,12 @@
 const eqArrays = function(array1, array2) {
-  let result = true;
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
+  let result = true; // assigning true to our result variable
+  if (array1.length === array2.length) { // if the lengths of arrays are equal..
+    for (let i = 0; i < array1.length; i++) {// loop through to then return true
+      if (array1[i] !== array2[i]) { // if unequal, return false
       return false;
       } 
     }
-    return result;
+    return result; // where our true result is executed
   } else {
     return false;
   }
